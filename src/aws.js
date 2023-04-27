d210e8eced1fff290d46f8a363546507e27f1ce4
arielly-parussulo <arielly.parussulo@quintoandar.com.br>
2023-04-27 20:42:54 -0300

    test placement tenancy

--- src/aws.js
+++ src/aws.js
@@ -46,6 +46,9 @@ async function startEc2Instance(label, githubRegistrationToken) {
     SecurityGroupIds: [config.input.securityGroupId],
     IamInstanceProfile: { Name: config.input.iamRoleName },
     TagSpecifications: config.tagSpecifications,
+    Placement: {
+      Tenancy: 'dedicated'
+    }
   };
 
   try {
